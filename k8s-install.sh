8410b23206d6e2732e408c22ee358a38c7bde58d
wrfly <mr.wrfly@gmail.com>
2018-04-22 21:51:54 +0800

    enhance: add questions

--- k8s-install.sh
+++ k8s-install.sh
@@ -7,17 +7,29 @@ function log() {
   echo "[$((`date +%s` - now ))] ## $@ ##"
 }
 
+# prepare
+MASTER="Y"
+SINGLE_MASTER="N"
+read -p "Install as a master node?: " -ei $MASTER MASTER
+if [[ "$MASTER" == "Y" ]];then
+  PRIMARY_IP=$(ip route get 8.8.8.8 | head -1 | awk '{print $7}')
+  read -p "The API server's address will be: " -ei $PRIMARY_IP PRIMARY_IP
+  read -p "Run this cluster as a single node?: " -ei $SINGLE_MASTER SINGLE_MASTER
+fi
+
 # install docker
 log "install docker"
 apt-get update
 apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+cat >/etc/apt/sources.list.d/docker.list <<EOF
+deb https://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable
+EOF
 apt-get -y update
 apt-get -y install docker-ce
 
-# congifure mirror and registries
-log "congifure mirror and registries"
+# congifure mirror and insecure registries
+log "congifure mirror and insecure registries"
 cat > /etc/docker/daemon.json <<EOF
 {
   "registry-mirrors": ["https://m.mirror.aliyuncs.com"],
@@ -26,25 +38,27 @@ cat > /etc/docker/daemon.json <<EOF
 EOF
 systemctl daemon-reload && systemctl restart docker
 
-
 # install k8s
 log "install k8s"
 apt-get install -y apt-transport-https curl
-
 ## I built a reverse proxy here, but you can use aliyun for better donwload speed
-# curl -s https://packagescloudgooglecoms.kfd.me/apt/doc/apt-key.gpg | apt-key add -
+# curl -fsSL https://packagescloudgooglecoms.kfd.me/apt/doc/apt-key.gpg | apt-key add -
 # cat >/etc/apt/sources.list.d/kubernetes.list <<EOF
-# deb http://packagescloudgooglecom.kfd.me/apt/ kubernetes-xenial main
+# deb http://packagescloudgooglecom.kfd.me/apt/ kubernetes-$(lsb_release -cs) main
 # EOF
 
-curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
 cat >/etc/apt/sources.list.d/kubernetes.list <<EOF
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-$(lsb_release -cs) main
 EOF
 
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 kubeadm completion bash > /etc/bash_completion.d/k8s
+
+# turn off swap for k8s doesn't support it
+log "turn off swap for k8s doesn't support it"
+swapoff -a && sed -i "s/exit/\# for k8s\nswapoff -a\nexit/g" /etc/rc.local
 
 # configure kubelet for downloading pause image
 log "configure kubelet for downloading pause image"
@@ -53,14 +67,12 @@ sed -i "s/ExecStart=$/Environment=\"KUBELET_EXTRA_ARGS=--pod-infra-container-ima
 log "and restart kubelet"
 systemctl daemon-reload && systemctl restart kubelet
 
-# turn off swap for k8s doesn't support it
-log "turn off swap for k8s doesn't support it"
-swapoff -a && sed -i "s/exit/\# for k8s\nswapoff -a\nexit/g" /etc/rc.local
+if [[ "$MASTER" != "Y" ]];then
+  log "install this node as a normal node, not master. exit."
+  exit 0
+fi
 
 # set k8s configuration
-log "set k8s configuration"
-PRIMARY_IP=$(ip route get 8.8.8.8 | head -1 | awk '{print $7}')
-read -p "The API server's address will be: " -ei $PRIMARY_IP PRIMARY_IP
 cat > kube-admin.conf <<EOF
 apiVersion: kubeadm.k8s.io/v1alpha1
 kind: MasterConfiguration
@@ -84,6 +96,5 @@ log "init calico network"
 wget 'u.kfd.me/2C8' -qO- | sed "s/quay.io/quay-io.mirror.kfd.me/g" > calico.yaml
 kubectl apply -f calico.yaml
 
-# uncaomment this line for running pods in master node,
-# if you only have one node
-# kubectl taint nodes --all node-role.kubernetes.io/master-
+[[ "$SINGLE_MASTER" == "Y" ]] && \
+  kubectl taint nodes --all node-role.kubernetes.io/master-
